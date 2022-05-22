0bc6841cbe40963c14fccbfbd75720e14b718343
ryan-1337 <ryanaouiche@gmail.com>
2022-05-22 17:40:34 +0200

    create index.php

--- www/html/.history/index_20220522173246.php
+++ www/html/.history/index_20220522173246.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
 </head>
-
 <body>
     <h1>Welcome to Docker AMPP</h1>
     <h2>What is Docker AMPP ?</h2>
     <h3>Apache MariaDB PHP Phpmyadmin</h3>
-    <ul>
-        <li><a href="http://localhost:273/phpinfo.php">phpinfo()</a></li>
-        <li><a href="http://localhost:8080">phpmyadmin</a></li>
-    </ul>
 </body>
-
 </html>
